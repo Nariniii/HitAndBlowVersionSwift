eaa0a4487f48717764254a45b40f47695ee028af
市川竜也 <tatsunari101@gmail.com>
2021-01-28 19:16:29 +0900

    Initial Commit

---
+++ Numeron/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Numeron
+//
+//  Created by Itikawa Tatsuya on 2021/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
